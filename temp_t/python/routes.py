041c7090a215e605b54fb731cb56720aaf176095
kinncillab <hsiang305@gmail.com>
2026-07-18 20:13:52 +0800

    照片

--- temp_t/python/routes.py
+++ temp_t/python/routes.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 
 # 建立一個 Blueprint
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    return render_template('index.html', title="首頁", message="hihi！")
+    return render_template('index.html')
+
+@main_bp.route('/image/<path:filename>')
+def serve_image(filename):
+    return send_from_directory('../image', filename)
